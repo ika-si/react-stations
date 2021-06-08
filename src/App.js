a82f7fc34bf54f795042ac9cb70ee5e289603ccc
ika-si <ikachan.0630@gmail.com>
2021-06-08 21:32:39 +0900

    station3

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ export const App = () => {
       <header>
         <p>Nikki</p>
       </header>
+      <p>道端で出会った犬</p>
+      <img src="https://images.dog.ceo/breeds/cairn/n02096177_1710.jpg"></img>
     </div>
   )
 }
