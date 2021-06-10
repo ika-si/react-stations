8a5cdb49bf07c7720cfd93a3a17f6d2674e3d030
ika-si <ikachan.0630@gmail.com>
2021-06-11 03:29:01 +0900

    station5

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ import './App.css'
  */
 export const App = () => {
 
-  const [url, setDogUrl] = useState("https://images.dog.ceo/breeds/cairn/n02096177_1710.jpg");
+  const [url, setDogUrl] = useState('https://images.dog.ceo/breeds/cairn/n02096177_1710.jpg');
+
+  const changeUrl = () => {
+    console.log("change")
+    setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")
+  }
 
   return (
     <div>
@@ -18,6 +23,8 @@ export const App = () => {
       </header>
       <p>道端で出会った犬</p>
       <img src={url} alt="dog"></img>
+      <br></br>
+      <button onClick={changeUrl}>更新</button>
     </div>
   )
 }
