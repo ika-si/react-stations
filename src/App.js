ca7b755ad48e11e3f0359181854f24969837e8ff
ika-si <ikachan.0630@gmail.com>
2021-06-11 02:40:08 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState } from "react";
 import './App.css'
-
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
+
+  const [url, setDogUrl] = useState("https://images.dog.ceo/breeds/cairn/n02096177_1710.jpg");
+
   return (
     <div>
       <header>
         <p>Nikki</p>
       </header>
       <p>道端で出会った犬</p>
-      <img src="https://images.dog.ceo/breeds/cairn/n02096177_1710.jpg"></img>
+      <img src={url} alt="dog"></img>
     </div>
   )
 }
